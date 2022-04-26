3e105709bb2c610767e6e9c001c571f52652f665
ShengWen <shengwen@bitstreetx.com>
2022-04-26 21:43:46 +0800

    [File] Added dcmtk library from official site

---
+++ dcmtk-3.6.6/dcmseg/tests/tests.cc
@@ -0,0 +1,30 @@
+/*
+ *
+ *  Copyright (C) 2015-2019, OFFIS e.V.
+ *  All rights reserved.  See COPYRIGHT file for details.
+ *
+ *  This software and supporting documentation were developed by
+ *
+ *    OFFIS e.V.
+ *    R&D Division Health
+ *    Escherweg 2
+ *    D-26121 Oldenburg, Germany
+ *
+ *
+ *  Module:  dcmseg
+ *
+ *  Author:  Michael Onken
+ *
+ *  Purpose: main test program
+ *
+ */
+
+#include "dcmtk/config/osconfig.h"
+
+#include "dcmtk/ofstd/oftest.h"
+
+OFTEST_REGISTER(dcmseg_utils);
+OFTEST_REGISTER(dcmseg_roundtrip);
+OFTEST_REGISTER(dcmseg_concat_binary);
+
+OFTEST_MAIN("dcmseg")
